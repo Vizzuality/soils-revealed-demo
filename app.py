9e5e593b19be9ae76da258ad3c26847819994973
Iker S. Requerey <iker.sanchez@vizzuality.com>
2023-06-08 14:00:10 +0100

    Load the environment variables

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ load_dotenv()
 
 # Initialize GEE
 private_key = os.getenv("EE_PRIVATE_KEY")
+st.write(type(private_key))
 #private_key = json.loads(os.getenv("EE_PRIVATE_KEY"))
 
 ee_credentials = ee.ServiceAccountCredentials(email=private_key['client_email'], key_data=json.dumps(private_key))
